80fd47438b0c4ad285b91ec8f86441b8cf612e36
liubov1211 <lyubov12111994@gmail.com>
2021-09-22 20:14:45 +0300

    change last line

--- home_work_1/script.js
+++ home_work_1/script.js
@@ -35,7 +35,7 @@ console.log(discount);
 const toPay = (sum - (sum * discount/100)).toFixed(2);
 console.log(toPay);
 
-const netProfit = Math.round(sum/2 - toPay);
+const netProfit = Math.round(toPay - sum/2);
 console.log(netProfit);
 
 const result = `Максимальна ціна: ${maxPrice},
