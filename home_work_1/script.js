bb38b7b0000080eee68f365e102cb062d7b42bcb
liubov1211 <lyubov12111994@gmail.com>
2021-09-24 21:48:53 +0300

    completion

--- home_work_1/script.js
+++ home_work_1/script.js
@@ -2,41 +2,31 @@ const applePrise = 15.678;
 const grapePrise = 123.965;
 const kiwiPrise = 90.2345;
 
-const maxPrice = Math.max(applePrise,grapePrise,kiwiPrise);
-const minPrice = Math.min(applePrise,grapePrise,kiwiPrise);
-console.log(maxPrice);
-console.log(minPrice);
-
+const maxPrice = Math.max(applePrise, grapePrise, kiwiPrise);
+const minPrice = Math.min(applePrise, grapePrise, kiwiPrise);
 
 const sum = applePrise + grapePrise + kiwiPrise;
-console.log(sum);
 
 const roundedApplePrise = Math.floor(applePrise);
 const roundedGrapePrise = Math.floor(grapePrise);
 const roundedKiwiPrise = Math.floor(kiwiPrise);
 const roundedSum = roundedApplePrise + roundedGrapePrise + roundedKiwiPrise;
-console.log(roundedSum);
 
-const roundedPrise = Math.round(roundedSum/100) * 100;
-console.log(roundedPrise);
+const roundedPrise = Math.round(roundedSum / 100) * 100;
 
-const check = (roundedSum%2 === 0);
-console.log("Парне -", check);
+const check = (roundedSum % 2 === 0);
 
-const surrender = 500 - sum;
-console.log(surrender);
+const money = 500;
+const surrender = money - sum;
 
 const midPrice = (sum / 3).toFixed(2);
-console.log(midPrice);
 
 const discount = Math.round(Math.random() * 100);
 console.log(discount);
 
-const toPay = (sum - (sum * discount/100)).toFixed(2);
-console.log(toPay);
+const toPay = (sum - (sum * discount / 100)).toFixed(2);
 
-const netProfit = Math.round(sum/2 - (sum * discount/100));
-console.log(netProfit);
+const netProfit = Math.round(sum / 2 - (sum * discount / 100));
 
 const result = `Максимальна ціна: ${maxPrice},
 Мінімальна ціна: ${minPrice},
